4f2a3b4c495246bd3472a0b7cbfa35235bed6867
pallat <yod.pallat@gmail.com>
2018-05-23 14:23:52 +0700

    cahnge 5

--- solution_test.go
+++ solution_test.go
@@ -15,3 +15,14 @@ func TestChange(t *testing.T) {
 		t.Errorf("expected %#v\nbut got %#v\n", expected, r)
 	}
 }
+
+func TestChange5(t *testing.T) {
+	r := Change(95, 100)
+	expected := map[float64]int{
+		5: 1,
+	}
+
+	if !reflect.DeepEqual(r, expected) {
+		t.Errorf("expected %#v\nbut got %#v\n", expected, r)
+	}
+}
